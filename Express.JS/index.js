e229043da95dba29ba3cbae34b0d7d48e69e64a0
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-01 13:08:18 -0300

    Criando nossa primeira rota para resolver erro cannot get /

--- Express.JS/index.js
+++ Express.JS/index.js
@@ -1,7 +1,19 @@
 const express = require("express"); // * Importando o Express no nosso arquivo raiz.
 const app = express(); // * Iniciando o Express.
 
-app.listen(2001,function(erro){ // Essa função que estamos criando é executada toda vez que o servidor é iniciado e ela recebe um parâmetro chamado "erro" então se ocorrer algum erro a gente recebe esse parâmetro.
+
+app.get("/", function(req,res){
+    res.send("Bem vindo ao meu site com Express")
+}); 
+// * Criando nossa primeira rota onde o primeiro parâmetro é o "nome da rota" e o segundo parâmetro geralmente definimos o que essa rota faz, então criamos uma função que possui aqueles dois parâmetros padrões (requisição e resposta). Em toda rota que criarmos temos que obrigatóriamente devolver uma resposta. Outro ponto importante sobre rotas é que você pode só pode enviar uma resposta uma UNICA VEZ.
+
+
+
+
+
+
+
+app.listen(2001,function(erro){ // * Essa função que estamos criando é executada toda vez que o servidor é iniciado e ela recebe um parâmetro chamado "erro" então se ocorrer algum erro a gente recebe esse parâmetro.
     if(erro){
         console.log("Ocorreu algum erro!")
     } else{
