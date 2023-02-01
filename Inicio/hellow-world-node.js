bbb7f1ea1379ea8984f20b5a1eeb3931b1a67245
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-01 11:16:45 -0300

    first commit

---
+++ Inicio/hellow-world-node.js
@@ -0,0 +1,10 @@
+var mostrarSite = true;
+const site = "www.aprendizadonode.com";
+console.log("\n" + "Hello World!");
+console.log("Meu nome é Guilherme");
+console.log("Estou aprendendo Node.JS");
+
+
+if(mostrarSite == true){ // ! se mostrar site.
+    console.log(site); // ! print o site.
+}
