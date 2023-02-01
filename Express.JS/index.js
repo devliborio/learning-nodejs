410c743a4b0c4ce016e7263b99d7ea75656e5f13
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-01 13:43:14 -0300

    Criando rota para aprender parâmetros

--- Express.JS/index.js
+++ Express.JS/index.js
@@ -3,7 +3,7 @@ const app = express(); // * Iniciando o Express.
 
 
 app.get("/", function(req,res){
-    res.send("Bem vindo ao meu site com Express");
+    res.send("<h2>Bem vindo ao meu site com Express</h2>");
 }); 
 // * Criando nossa primeira rota onde o primeiro parâmetro é o "nome da rota" e o segundo parâmetro geralmente definimos o que essa rota faz, então criamos uma função que possui aqueles dois parâmetros padrões (requisição e resposta). Em toda rota que criarmos temos que obrigatóriamente devolver uma resposta. Outro ponto importante sobre rotas é que você pode só pode enviar uma resposta uma UNICA VEZ.
 
@@ -14,6 +14,13 @@ app.get("/blog", function(req,res){
 app.get("/canal/youtube", function(req,res){
     res.send("Bem vindo ao meu canal!")
 });
+
+app.get("/ola/:nome", function(req,res){
+    // REQ => DADOS ENVIADOS PELO USUARIO
+    // RES => RESPOSTA QUE VAI SER ENVIADA PARA O USUARIO.
+    var nome = req.params.nome
+    res.send("<h1>Oi " + nome + "</h1>");
+})
 
 app.listen(2001,function(erro){ // * Essa função que estamos criando é executada toda vez que o servidor é iniciado e ela recebe um parâmetro chamado "erro" então se ocorrer algum erro a gente recebe esse parâmetro.
     if(erro){
