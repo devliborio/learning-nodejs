7cb639482dde1adbda661385650fa45880c13a32
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-01 12:43:28 -0300

    Criando estrutura inicial para abrir um servidor express

--- Express.JS/index.js
+++ Express.JS/index.js
@@ -1,2 +1,10 @@
 const express = require("express"); // * Importando o Express no nosso arquivo raiz.
 const app = express(); // * Iniciando o Express.
+
+app.listen(2001,function(erro){ // Essa função que estamos criando é executada toda vez que o servidor é iniciado e ela recebe um parâmetro chamado "erro" então se ocorrer algum erro a gente recebe esse parâmetro.
+    if(erro){
+        console.log("Ocorreu algum erro!")
+    } else{
+        console.log("Servidor Rodando Normalmente")
+    }
+})
