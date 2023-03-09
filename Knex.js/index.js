147464e49d86f1c1aaeffbb291aeb11aec2bab90
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-09 16:52:26 -0300

    Enviando JOIN com query builder

--- Knex.js/index.js
+++ Knex.js/index.js
@@ -1,42 +1,41 @@
 //** Para saber a forma crua de um comando do knex use: console.log(query.toQuery());
 const database = require("./database");
 
-/* 
+/*
 //! Usando o comando INSERT com query builder!
 let dados = [
         
         {
-            nome: "God of war",
-            preco: 200
+            nome: "Ark survival",
+            preco: 99.99
         },
 
         {
-            nome: "Uncharted",
-            preco: 150
+            nome: "Bayonetta",
+            preco: 80
         },
 
         {
-            nome: "GTA V",
-            preco: 30.50
+            nome: "the forest",
+            preco: 35.45
         },
 
         {
-            nome: "Far cry 3",
+            nome: "Far cry - New Dawn",
             preco: 26.89
         }
 ]
 
 
-database.insert(dados).into("games")
+let query1 = database.insert(dados).into("games")
                        .then(data => console.log(data))
                           .catch(err => console.log(err));
-
 */
 
 
 /*
 //! Usando o comando SELECT com query builder!
-database.select("*").table('games')
+let query2 = database.select("*").table('games')
                         .then(data => console.log(data);
                           .catch(err => console.log(err));
 */
@@ -44,13 +43,13 @@ database.select("*").table('games')
 
 /*
 //! Usando o comando WHERE com query builder!
-let query = database.select(["nome", "preco"])
+let query3 = database.select(["nome", "preco"])
                         .where({nome: "GTA V"})
                             .where({id: 14})  // Na forma crua ele adiciona automaticamente o 'and'
                                 .table("games").then(data => console.log(data)).catch(err => console.log(err));
 
 
-let query2 = database.select(["nome", "preco"])
+let query4 = database.select(["nome", "preco"])
                              .whereRaw("nome = 'Far cry 3' && preco > 10") // Para realizar operações condicionais como (!=, <=...) 
                                 .table("games").then(data => console.log(data)).catch(err => console.log(err));
 
@@ -61,7 +60,7 @@ let query2 = database.select(["nome", "preco"])
 
 /*
 //! Caso você queira fazer uma query que o knexjs não te dê muita flexibilidade, você pode usar uma função do knex chamada raw que deixa você criar comandos SQL de forma crua, como é demonstrado abaixo:
-database.raw("SELECT * FROM games")
+let query5 = database.raw("SELECT * FROM games")
             .then(data => console.log(data))
                 .catch(err => console.log(err));
 */
@@ -69,7 +68,7 @@ database.raw("SELECT * FROM games")
 
 /*
 //! Usando o comando DELETE com query builder
-let query3 = database.where({id: 12})
+let query6 = database.where({id: 12})
                         .delete()
                             .table("games")
                                 .then(data => console.log(data))
@@ -80,9 +79,49 @@ let query3 = database.where({id: 12})
 
 /*
 //! Usando o comando UPDATE  com query builder
-let query4 = database.where({ id: 14})
+let query7 = database.where({ id: 14})
                         .update({ preco: 170 })
                             .table("games")
                                 .then(data => console.log(data))
                                     .catch(err => console.log(err));
 */
+
+/*
+//! Usando o Order By para ordenar os dados da sua query
+let  query8 = database.select()
+                        .table("games")
+                            .orderBy("preco", "desc") // Decrescente = desc, Crescente = asc
+                                .then(data => console.log())
+                                    .catch(err => console.log(err));
+
+let query9 = database.select()
+                        .table("games")
+                            .orderBy("nome", "asc")
+                                .then(data => console.log(data))
+                                    .catch(err => console.log(err));
+*/
+
+
+/*
+//! Criando INSERT's associados que é basicamente você inserir um registro que tem relacionamento com outro registro
+database.insert({nome: "Ubisoft",game_id: 15})
+            .table("estudios")
+                .then(data => console.log(data))
+                    .catch(err => console.log(err));
+*/
+
+
+//! Usando JOIN para relacionamentos de tabelas
+database.select(["estudios.id as estudio_id", "estudios.nome as estudio_nome", "games.id", "games.nome"]) // O "as" renomeia o campo caso tenha conflito de nomes nas tabelas.
+            .table("games")
+                .innerJoin("estudios", "estudios.game_id", "games.id")
+                    .then(data => console.log())
+                        .catch(err => console.error(err));  
+
+//! Usando JOIN com WHERE em relacionamentos de tabelas
+database.select("games.*", "estudios.nome as estudio_nome")
+            .table("games")
+                .innerJoin("estudios", "estudios.game_id", "games.id")
+                    .where("games.id", 15)
+                        .then(data => console.log(data))
+                            .catch(err => console.error(err));
