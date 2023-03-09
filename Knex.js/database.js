e4b1a69f5b8a5306e46f914ba5b8f55d40e1a07e
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-09 14:00:06 -0300

    reconfigurando knex

--- Knex.js/database.js
+++ Knex.js/database.js
@@ -1,11 +1,12 @@
-let knex = require('knex')({
+const knex = require('knex')({
     client: 'mysql2',
     connection: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'knexjs'
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : 'root',
+      database : 'knexjs'
     }
-});
+  });
 
 module.exports = knex;
