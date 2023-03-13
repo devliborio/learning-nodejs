1346631eeea99b2c3a58de702bf8a98586277ea1
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-13 14:10:42 -0300

    Novo componente de assinatura

---
+++ vueclientes/src/components/Assinatura.vue
@@ -0,0 +1,46 @@
+<template>
+    
+    <div id="sign">
+        <h3>Planos de Assinatura</h3>
+
+        <p>Assinatura: {{titleD}}</p>
+        <p>Por apenas: <span class="price_item"> R$ {{priceD}} </span> de forma {{type}}</p>
+        
+        <hr>
+
+        <p>Assinatura: {{titleN}}</p>
+        <p>Por apenas: <span class="price_item"> R$ {{priceN}} </span> de forma {{type}}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+    data(){
+
+        return{
+
+            titleD: 'Disney+',
+            titleN: 'Netflix',
+            priceD: 60.00,
+            priceN: 49.90,
+            type: 'mensal',
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+    .price_item{
+        font-weight: bold;
+    }
+
+    #sign{
+        font-family: sans-serif;
+        padding: 1%;
+    }
+
+</style>
