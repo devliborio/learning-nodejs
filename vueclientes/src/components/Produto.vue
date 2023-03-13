62680e5e3178bf19386cffb7af96adb2d029f3d1
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-13 11:13:07 -0300

    Usando data binding para definir uma variavel dentro de um atributo

--- vueclientes/src/components/Produto.vue
+++ vueclientes/src/components/Produto.vue
@@ -2,8 +2,12 @@
   <div id="produtos">
 
     <p>Caso queira ser assinante do nosso plano de membros {{nome}}, você só pagará um preço exclusivo e limitado de <span class="price">${{preco}}</span> sendo na modalidade {{tempo}}</p>
-    <button type="submit">Assinar agora!!</button>
 
+    <p>Para ser um assinante envie seu e-mail através do formulário abaixo e depois clique em assinar para aproveitar os 14 dias de teste gratis!</p>
+
+    <input type="email" :value="email"><br><br> <!-- Para usar uma variável do JS dentro do atributo de algum input de formulário use o ":" na frente do seu atributo e insira o a variável dentro do atributo  -->
+
+    <button type="submit">Assinar!</button>
   </div>
 </template>
 
@@ -13,7 +17,8 @@ export default {
     return {
         nome: "UserHelp Plus+",
         preco: 50.99,
-        tempo: "mensal"
+        tempo: "mensal",
+        email: "liborio.ofc@gmail.com"
     };
   },
 };
