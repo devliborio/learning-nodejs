1b70d15d8cea6a619f5d0fe641be22dae77a0938
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-13 10:17:41 -0300

    Entendendo interpolação na pratica

--- vueclientes/src/components/Cliente.vue
+++ vueclientes/src/components/Cliente.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="cliente">
-    <h2 id="cliente_nome">Cliente aqui - Componente aqui!</h2>
-    <h3>Descrição do cliente: bla! bla! bla!</h3>
-    <hr>
+    <p> <span class="sp_1"> | QA |: </span> Olá {{ nome }}, seja bem vindo, vimos que sua idade de {{idade}} corresponde a nossas expectativas, ser um {{profissao}} vai muito além de talento! </p>
+    <!-- SIMBOLO DE INTERPOLAÇÃO! -->
+    <hr />
+    <p> <span class="sp_2"> | You |: </span> Muito obrigado pelo retorno, fico muito feliz em agregar para o time, por favor ligue para o meu número {{numero}}, é uma honra ser escolhido por vocês!  </p>
   </div>
 </template>
 
 <script>
 export default {
   // Quando você cria um componente você precisa exportar ele para que ele seja usado em outro componentes se for o seu caso!
+  data() {
+    return {
+      nome: "Guilherme Libório Machado",
+      idade: 21,
+      profissao: "Full Stack Developer",
+      numero: 7198666999
+    };
+  },
 };
 </script>
 
 
 <style scoped>
 /*Esse scoped define que esse estilo vai ser aplicado somente para esse componente em especifico e não aos outros da pagina!*/
+  .sp_1, .sp_2{
+    font-weight: bold;
+  }
+
+  #cliente p{
+    font-family: sans-serif;
+  }
 </style>
